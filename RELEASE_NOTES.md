788ec8a16633248f45e8c2b0e0d300458305c332
Evelina Gabasova <egabasova@gmail.com>
2014-09-14 21:17:15 +0100

    Fix .nuspec file

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
 #### 0.1.0 - 14 September 2014
 * Initial release
+
+#### 0.1.1 - 14 September 2014
+* Fix logo in NuGet
--- src/Ariadne/AssemblyInfo.fs
+++ src/Ariadne/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Ariadne")>]
 [<assembly: AssemblyProductAttribute("Ariadne")>]
 [<assembly: AssemblyDescriptionAttribute("F# package for Gaussian process regression.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.1.1"
