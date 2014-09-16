3bde8bfbd47834f374d22ce5e9383ef71665c85d
Evelina Gabasova <egabasova@gmail.com>
2014-09-17 00:24:24 +0100

    Bump version to 0.1.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -3,3 +3,6 @@
 
 #### 0.1.1 - 14 September 2014
 * Fix logo in NuGet
+
+#### 0.1.2 - 17 September 2014
+* Adding link to an example application to the documentation
--- src/Ariadne/AssemblyInfo.fs
+++ src/Ariadne/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Ariadne")>]
 [<assembly: AssemblyProductAttribute("Ariadne")>]
 [<assembly: AssemblyDescriptionAttribute("F# package for Gaussian process regression.")>]
-[<assembly: AssemblyVersionAttribute("0.1.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] Version = "0.1.2"
