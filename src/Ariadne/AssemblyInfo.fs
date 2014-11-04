52713c70b5c265fbf5494dcd72b62aa2290cf200
Evelina Gabasova <egabasova@gmail.com>
2014-11-04 21:46:50 +0000

    Bump version to 0.1.4

--- src/Ariadne/AssemblyInfo.fs
+++ src/Ariadne/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Ariadne")>]
 [<assembly: AssemblyProductAttribute("Ariadne")>]
 [<assembly: AssemblyDescriptionAttribute("F# package for Gaussian process regression.")>]
-[<assembly: AssemblyVersionAttribute("0.1.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.3")>]
+[<assembly: AssemblyVersionAttribute("0.1.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.3"
+    let [<Literal>] Version = "0.1.4"
